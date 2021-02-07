c160cb93eeb7ef3d83cfa27a6fd9d010c177c328
shihab17 <56069831+shihab17@users.noreply.github.com>
2021-02-07 23:23:40 +0600

    asignment done

--- script.js
+++ script.js
@@ -1,48 +1,66 @@
-// function handleErrors(res) {
-//     if (!res.ok) throw new Error(res.error);
-//     return res;
-//   }
-const handleErrors = (res)=>{
+const searchMeal = () => {
+    const shearchedMeal = document.getElementById("shearchingMeal").value;
+    document.getElementById("shearchingMeal").value = "";
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${shearchedMeal}`)
+        .then(handleErrors)
+        .then(res => res.json())
+        .then(data => meals(data.meals))
+        .catch(err => {
+            document.getElementById("errorHandale").style.display = "block";
+            document.getElementById('error').innerText = "No Data Found";
+        });
+}
+const handleErrors = (res) => {
     if (!res.ok) throw new Error(res.error);
     return res;
 }
-const searchMeal = () => {
-    const category = document.getElementById("shearchingMeal").value;
-    document.getElementById("shearchingMeal").value="";
-    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`)
-    .then(handleErrors)
-    .then(res=> res.json())
-    .then(data=> meals(data.meals))
-    .catch(err => console.log("err"));
-}
-// function searchMeal(){
-//     const category = document.getElementById("shearchingMeal").value;
-//     document.getElementById("shearchingMeal").value="";
-//     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`)
-//     .then(handleErrors)
-//     .then(res=> res.json())
-//     .then(data=> meals(data.meals))
-//     .catch(err => console.log("err"));
-    
-// }
 const meals = (meals) => {
     meals.forEach(meal => {
-        // console.log(meal);
         const masterDiv = document.getElementById("master");
         const mealDiv = document.createElement("div");
-        const test = `
-        <img onclick="test()" src="${meal.strMealThumb}" alt="">
-        <h3 onclick="test()">${meal.strMeal}</h3>
-        `
-        
-        mealDiv.innerHTML=test;
+        const showMeal = `
+        <div class="meal"> 
+        <img onclick="mealDetails(${meal.idMeal})" src="${meal.strMealThumb}" alt="">
+        <h5 onclick="mealDetails(${meal.idMeal})">${meal.strMeal}</h5>
+        </div>`
+        mealDiv.innerHTML = showMeal;
         masterDiv.appendChild(mealDiv);
     });
 }
-
-function test(){
-    console.log("clicked")
+const mealDetails = (mealId) => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
+        .then(res => res.json())
+        .then(data => {
+            meal(data.meals[0]);
+        })
 }
 
-{/* <img onclick="test()" src="${meal.strMealThumb}" alt="">
-<h3 onclick="test()">${meal.strMeal}</h3> */}
+const meal = (detailMeal) => {
+    const mealDetails = document.getElementById('mealDetails');
+    mealDetails.style.display = "block";
+    const h2 = document.getElementById('mealName');
+    h2.innerText = detailMeal.strMeal;
+    document.getElementById('mealThumb').src = detailMeal.strMealThumb;
+    // list of Ingredients
+    document.getElementById('strMeasure1').innerText = detailMeal.strMeasure1;
+    document.getElementById('strMeasure2').innerText = detailMeal.strMeasure2;
+    document.getElementById('strMeasure3').innerText = detailMeal.strMeasure3;
+    document.getElementById('strMeasure4').innerText = detailMeal.strMeasure4;
+    document.getElementById('strMeasure5').innerText = detailMeal.strMeasure5;
+    document.getElementById('strMeasure6').innerText = detailMeal.strMeasure6;
+    document.getElementById('strMeasure7').innerText = detailMeal.strMeasure7;
+    document.getElementById('strMeasure8').innerText = detailMeal.strMeasure8;
+    document.getElementById('strMeasure9').innerText = detailMeal.strMeasure9;
+    document.getElementById('strMeasure10').innerText = detailMeal.strMeasure10;
+    document.getElementById('strMeasure11').innerText = detailMeal.strMeasure11;
+    document.getElementById('strMeasure12').innerText = detailMeal.strMeasure12;
+    document.getElementById('strMeasure13').innerText = detailMeal.strMeasure13;
+    document.getElementById('strMeasure14').innerText = detailMeal.strMeasure14;
+    document.getElementById('strMeasure15').innerText = detailMeal.strMeasure15;
+    document.getElementById('strMeasure16').innerText = detailMeal.strMeasure16;
+    document.getElementById('strMeasure17').innerText = detailMeal.strMeasure17;
+    document.getElementById('strMeasure18').innerText = detailMeal.strMeasure18;
+    document.getElementById('strMeasure19').innerText = detailMeal.strMeasure19;
+    document.getElementById('strMeasure20').innerText = detailMeal.strMeasure20;
+}
+
